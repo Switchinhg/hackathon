39935bf48ef2668594bbd9505d4fbfcfd36a5395
Switchinhg <santifonlop@hotmail.com>
2023-09-13 17:52:47 -0300

    added views: login, index + routes +  changed readme, index.html

---
+++ src/components/Login.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className='d-flex justify-content-center align-items-center' style={{height:'calc(100vh - 117px)'}}>
+                <button className='btn btn-primary mb-5'>Login with Github</button>        
+    </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import "./index.css";
 import App from "./App";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import Header from "./components/Header";
+import Login from "./components/Login";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Index from "./components/Index";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
 
@@ -15,7 +17,9 @@ ReactDOM.render(
           <Header/>
 
           <Routes>
+            <Route path="/" element={<Index />}/>
             <Route path="/app" element={<App />}/>
+            <Route path="/login" element={<Login />}/>
           </Routes>
         </Router>
     </ConvexProvider>
